8b92d97d130745cfb9ee52b9c88c901282c64872
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-01-19 16:40:30 +0700

    restructure folders, add Assembly Definition files. Remove FAN and Unity ads helper (will be made into its own repo as a plugin)

--- OmniAdsManager/FANManager/FANEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using AudienceNetwork;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class FANEventHandler
-{
-    public static FBAdViewBridgeCallback adViewDisposed;
-}
