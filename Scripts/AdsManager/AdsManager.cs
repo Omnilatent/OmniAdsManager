1e86d74706e39285e432df89d5b9262c421145a1
SangVu <sangvd@jacat.games>
2023-11-07 15:52:22 +0700

    change time invoke on app open ads request

--- Scripts/AdsManager/AdsManager.cs
+++ Scripts/AdsManager/AdsManager.cs
@@ -730,7 +730,6 @@ public partial class AdsManager : MonoBehaviour
 
     IEnumerator CoRequestAppOpenAd(AdPlacement.Type placementType, InterstitialDelegate onAdLoaded = null, bool showLoading = false)
     {
-        OnAppOnAdRequestEvent.Invoke(placementType);
         isLoadingAppOpenAd = true;
         bool isSuccess = false;
         WaitForSecondsRealtime checkInterval = new WaitForSecondsRealtime(0.05f);
@@ -765,6 +764,7 @@ public partial class AdsManager : MonoBehaviour
 
     public void ShowAppOpenAd(AdPlacement.Type placementType, InterstitialDelegate onAdClosed = null)
     {
+        OnAppOnAdRequestEvent.Invoke(placementType);
         //.Log($"showingAppOpenAd:{showingAppOpenAd}, showingRewardAd:{showingRewardAd}, showingInterstitial:{ShowingInterstitial}, LoadingActive:{LoadingActive}");
         if (DoNotShowAds(placementType) || !HasEnoughTimeBetweenAppOpenAd() || showingAppOpenAd || showingRewardAd || ShowingInterstitial
             || LoadingActive) //to prevent app open ad to show behind interstitial when an interstitial is loading
