88307d3d8f939c12cc11e4bd44a00ca78c70618b
Trong Nguyen <nguyentrong101094@gmail.com>
2024-02-06 11:01:16 +0700

    Set screen block cancelable after a short amount of time as a way to prevent game stuck.

--- Scripts/ScreenBlock.cs
+++ Scripts/ScreenBlock.cs
@@ -24,13 +24,17 @@ namespace Omnilatent.AdsMediation
             initialized = true;
         }
 
-        public static void Show()
+        /// <summary>
+        /// Show a black screen with the text "Loading ad" to cover the whole screen and the banner ad.
+        /// </summary>
+        /// <param name="cancelableDelay">The amount of miliseconds to wait before setting the black screen cancelable to true to allow use Back button to close it in case it's not closed by callback. Set to -1 if you don't need it.</param>
+        public static void Show(int cancelableDelay = 4000)
         {
             #if UNITY_ANDROID && !UNITY_EDITOR
             try
             {
                 Initialize();
-                _pluginInstance.CallStatic("showDialog", unityActivity);
+                _pluginInstance.CallStatic("showDialog", unityActivity, cancelableDelay);
             }
             catch (Exception e)
             {
